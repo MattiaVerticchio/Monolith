f7f82e684b4e9a7605ca8b03a3b3c01da8d5ee35
Mattia Verticchio <git.urel1@simplelogin.co>
2024-09-19 23:09:25 +0200

    Implement cat

--- Compiler/Source/Main.elm
+++ Compiler/Source/Main.elm
@@ -1,8 +1,18 @@
 module Main exposing (run)
 
+import BackendTask exposing (BackendTask)
+import BackendTask.File as File
+import FatalError exposing (FatalError)
 import Pages.Script as Script exposing (Script)
 
 
 run : Script
 run =
-    Script.withoutCliOptions (Script.log "Hello, I am the compiler")
+    Script.withoutCliOptions compile
+
+
+compile : BackendTask FatalError ()
+compile =
+    File.rawFile "test.🗿"
+        |> BackendTask.allowFatal
+        |> BackendTask.andThen Script.log
